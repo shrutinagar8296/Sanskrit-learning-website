8f9b7129f20032feac28f9f278a5b2f69bc1fad0
shrutinagar8296 <118724545+shrutinagar8296@users.noreply.github.com>
2022-12-30 16:15:33 +0530

    Delete home/EducationTool directory

--- home/EducationTool/index.js
+++
@@ -1,6 +0,0 @@
-function DisplayMeOptions() {
-	if(document.getElementById('quizContentdisplay').style.display == 'block')
-		 document.getElementById('quizContentdisplay').style.display ="none";
-	else 
-		 document.getElementById('quizContentdisplay').style.display = "block";
- }
